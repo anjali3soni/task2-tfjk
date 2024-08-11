062594442b140612d6fccd008473d7911d08d11e
Dhruv-Bagora <https://github.com/Dhruv-Bagora>
2024-08-11 23:28:36 +0530

    One more change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Dhruv Bagora's React app.
+          This is Dhruv Bagora's React app for testing purpose.
         </p>
         <a
           className="App-link"
